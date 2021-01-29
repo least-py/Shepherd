783711b1c2272df9fb0ceb167758d6299fa57076
Maython <lukas_neu@stricker-dreisen.de>
2021-01-29 22:21:34 +0100

    Camera

--- Shepherd/Assets/Scripts/SC_CharacterController.cs
+++ Shepherd/Assets/Scripts/SC_CharacterController.cs
@@ -11,6 +11,8 @@ public class SC_CharacterController : MonoBehaviour
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
 
+
+
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
     Vector2 rotation = Vector2.zero;
@@ -48,7 +50,9 @@ public class SC_CharacterController : MonoBehaviour
         // Player and Camera rotation
         if (canMove)
         {
-
+            rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+            rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(23f, 0, 0);
             transform.eulerAngles = new Vector2(0, rotation.y);
         }
